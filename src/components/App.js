0dd2bf96303f474f0d9d11aec09c47b356cfc51c
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-08-10 13:30:47 +0200

    changing toggleMap

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
-import { styled } from '@material-ui/core/styles';
+import { styled, makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import EspanaMapa from '../mapas/España.svg';
 import AndaluciaMapa from '../mapas/Andalucía-a.svg';
@@ -14,9 +15,39 @@ import Espana from './ui/comunidades/Espana';
 import Andalucia from './ui/comunidades/Andalucia';
 import Aragon from './ui/comunidades/Aragon';
 
+const useStyles = makeStyles(() => ({
+  sideBar: {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+}));
+
 function App() {
-  const [mapas, showMapas] = useState(true);
-  const toggleMapas = () => showMapas(!mapas);
+  // const [mapas, showMapas] = useState(true);
+  // const toggleMapas = () => showMapas(!mapas);
+
+  const Inicio = () => {
+    const [showEspana, setShowEspana] = useState(true);
+    const [showAndalucia, setShowAndalucia] = useState(false);
+    const [showAragon, setShowAragon] = useState(false);
+    const classes = useStyles();
+
+    const toggleMapas = mapName => {
+      if (mapName === 'Andalucia') {
+        setShowEspana(false);
+        setShowAndalucia(true);
+        setShowAragon(false);
+      } else if (mapName === 'Aragon') {
+        setShowEspana(false);
+        setShowAndalucia(false);
+        setShowAragon(true);
+      } else {
+        setShowEspana(true);
+        setShowAndalucia(false);
+        setShowAragon(false);
+      }
+    };
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -31,12 +62,22 @@ function App() {
         </Switch>
       </BrowserRouter>
       <div>
-        <Espana />
-        <img src={EspanaMapa} alt="España"></img>
-        <Andalucia onClick={toggleMapas} />
-        {mapas && <img src={AndaluciaMapa} alt="Andalucia"></img>}
-        <Aragon onClick={toggleMapas} />
-        {mapas && <img src={AragonMapa} alt="Aragon"></img>}
+        <Grid container>
+          <Grid item xs={1}>
+            <div>
+              <Espana onClick={() => toggleMapas('Espana')} />
+              <Andalucia onClick={() => toggleMapas('Andalucia')} />
+              <Aragon onClick={() => toggleMapas('Aragon')} />
+            </div>
+          </Grid>
+          <Grid item xs={11}>
+            <img src={EspanaMapa} alt="España"></img>
+
+            {mapas && <img src={AndaluciaMapa} alt="Andalucia"></img>}
+
+            {mapas && <img src={AragonMapa} alt="Aragon"></img>}
+          </Grid>
+        </Grid>
       </div>
     </ThemeProvider>
   );
